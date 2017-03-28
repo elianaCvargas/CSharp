eb3f8923c90d0c6ee9f9f4ac02ca8e071da7423b
elianaCvargas <carla.vargas.e@gmail.com>
2017-03-27 22:15:42 -0300

    Add files via upload

---
+++ tareaLabII/tareaLabII/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tareaLabII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Rueda ruedaUno = new Rueda();
+            Rueda ruedaDos = new Rueda();
+            ruedaUno.marca = "firestone";
+            ruedaUno.tamaño = 165;
+            ruedaDos.marca = "peugeot";
+            ruedaDos.tamaño = 185;
+
+            Console.WriteLine("Rueda Uno: " + ruedaUno.mostrar());
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ tareaLabII/tareaLabII/Rueda.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tareaLabII
+{
+    class Rueda
+    {
+        public string marca;
+        public int tamaño;
+
+        public string mostrar()
+        {
+
+            return this.marca +" " + this.tamaño.ToString();
+
+            
+        }
+
+
+    }
+}
